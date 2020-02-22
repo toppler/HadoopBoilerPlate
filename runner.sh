9a84c052959a59901a7e2d6acadbe0a29bdc1915
zerompx <henok.alene@gmail.com>
2020-02-21 20:41:30 -0800

    clean commit msg adding new files

--- runner.sh
+++ runner.sh
@@ -1,3 +1,8 @@
+#need to know base
+#TBD
 rm -rf output
 hadoop com.boilerplate.Application input/docmapping output
+<<<<<<< HEAD
 # call csv creator after this
+=======
+>>>>>>> Combined msg for the work commit
